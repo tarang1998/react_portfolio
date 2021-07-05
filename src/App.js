19e7ef2bf12bae8808a440a2a9873a5b9ae4aef8
Tarang <tarangnair98@gmail.com>
2021-07-05 08:59:12 +0530

    update - created header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Navigation from './navigation/Navigation';
+import Header from './header/Header'; 
+
 
 function App() {
   return (
     <div className="App">
-     
+
+      <Navigation logoTitle = 'Tarang Nair'/>
+
+      <Header/>
+
     </div>
   );
 }
--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -1,17 +1,18 @@
 import './Navigation.css';
 
-function Navigation() {
+function Navigation(data) {
+  const section = ['Home', 'About' , 'Services' ,'Portfolio', 'Contact']
+
+  const navLinks = section.map( section => {
+    return <li><a href={'#'+ section}>{section}</a></li>
+  });
+
   return (
     <nav>
-        <h2 className='logo'>React Portfolio</h2>
+        <h2 className='logo'>{data.logoTitle}</h2>
 
         <ul>
-            <li><a href='#'>Home</a></li>
-            <li><a href='#'>About</a></li>
-            <li><a href='#'>Services</a></li>
-            <li><a href='#'>Portfolio</a></li>
-            <li><a href='#'>Contact</a></li>
-            
+            {navLinks}
         </ul>
     </nav>
   );
