8bc6cb0cc943006076181d7aad637865742f9bfb
Tarang <tarangnair98@gmail.com>
2021-07-18 23:48:53 +0530

    Created various components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 //import ReactPortfolio from './react_portfolio_0/ReactPortfolio';
+import ReactPortfolio from './react_portfolio_1/ReactPortfolio'
 
 
 
@@ -7,7 +8,7 @@ function App() {
   return (
     <div className="App">
 
-      {/* <ReactPortfolio/> */}
+      <ReactPortfolio/>
 
     </div>
   );
