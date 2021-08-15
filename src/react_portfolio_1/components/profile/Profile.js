cf08f654702c5f897fb4a6e41127f996f244a969
Tarang <tarangnair98@gmail.com>
2021-08-15 18:52:01 +0530

    Created a profile page

--- src/react_portfolio_1/components/profile/Profile.js
+++ src/react_portfolio_1/components/profile/Profile.js
@@ -1,9 +1,29 @@
+import { Typography } from '@material-ui/core'
 import React from 'react'
+
+import './Profile.css';
 
 const Profile = () => {
     return (
-        <div>
-            This is a profile component 
+        <div className = 'profile container_shadow'>
+            <div className = 'profile_name'>
+                <Typography className ='name'>
+                    Tarang Nair
+                </Typography>
+                <Typography className ='title'>
+                    Software Engineer
+                </Typography>
+            </div>
+
+            <figure className ='profile_image'>
+                <img src={require('../../assets/images/tarang2.jpeg')} alt='' />
+            </figure>
+
+            <div className='profile_information'>
+                Insert Timeline
+                <br/>
+                <button>My button</button>
+            </div>
         </div>
     )
 }
