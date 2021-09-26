fb724303eddf5d47b92b63877ab7f47bde9768d2
Tarang <tarangnair98@gmail.com>
2021-09-26 18:06:34 +0530

    update - timeline

--- src/react_portfolio_1/components/profile/Profile.js
+++ src/react_portfolio_1/components/profile/Profile.js
@@ -1,5 +1,6 @@
 import { Typography } from '@material-ui/core'
 import React from 'react'
+import CustomTimeline from '../timeline/Timeline'
 
 import './Profile.css';
 
@@ -20,7 +21,7 @@ const Profile = () => {
             </figure>
 
             <div className='profile_information'>
-                Insert Timeline
+                <CustomTimeline/>
                 <br/>
                 <button>My button</button>
             </div>
