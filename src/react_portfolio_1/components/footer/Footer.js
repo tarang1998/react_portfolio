9b2f799c3260a5b9249a724722b4b141c1e571bb
Tarang <tarangnair98@gmail.com>
2021-10-19 19:48:20 +0530

    Created the footer component

--- src/react_portfolio_1/components/footer/Footer.js
+++ src/react_portfolio_1/components/footer/Footer.js
@@ -1,9 +1,25 @@
+import { Typography } from '@material-ui/core'
 import React from 'react'
+import {Favorite} from '@material-ui/icons'
+
+import './Footer.css'
+import resumeData from '../../utils/resumeData';
 
 const Footer = () => {
     return (
-        <div>
-            Footer
+        <div className='footer'>
+
+            <div className = 'footer_left'>
+                <Typography className='footer_name'><a href={resumeData.socials['Linkedin'].link} target='_blank'>Lets Connect!</a></Typography>
+            </div> 
+
+            <div className = 'footer_right'>
+                <Typography className='footer_copyright'>
+                    Developed with <Favorite></Favorite> by <a href={resumeData.socials['Github'].link} target='_blank'>{resumeData.name}</a>
+                </Typography>
+            </div> 
+
+
         </div>
     )
 }
