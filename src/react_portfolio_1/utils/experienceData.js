c2f3b93995666257419fe590221d595e85eb1dd6
Tarang <tarangnair98@gmail.com>
2022-01-23 22:12:52 +0530

    Creating experience data information

--- src/react_portfolio_1/utils/experienceData.js
+++ src/react_portfolio_1/utils/experienceData.js
@@ -2,9 +2,27 @@ export const experience = {
 
     title : "Experience",
     subtitle : "Work and Internships",
-    description : "💼 I'm currently working as a Back End Engineer at ",
-    currentCompanyName : "Cerebranium",
-    currentCompanyWebsiteLink : "https://cerebranium.com/"
+
+    description : `💼 I'm currently working as a Back End Engineer at `,
+    description1 : "💻 Open to work on freelancing Projects.",
+
+    currentCompanyName : "Cerebranium.",
+    currentCompanyWebsiteLink : "https://cerebranium.com/",
+
+    sections: [
+        {
+          title: "Work Experience",
+          experiences: [
+           
+          ],
+        },
+        {
+          title: "Internships",
+          experiences: [
+            
+          ],
+        },
+      ],
 
 }
 
