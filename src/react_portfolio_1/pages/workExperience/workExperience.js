805bfc8e77fcfd0ce06f68c570478673b7779905
Tarang <tarangnair98@gmail.com>
2022-01-23 14:48:36 +0530

    wip - work experience page

--- src/react_portfolio_1/pages/workExperience/workExperience.js
+++ src/react_portfolio_1/pages/workExperience/workExperience.js
@@ -1,10 +1,54 @@
+import { Grid } from '@material-ui/core'
 import React from 'react'
+import ExperienceImg from '../../utils/experienceSVGs/experienceImg'
+import { useContext } from 'react'
+import { ThemeContext } from 'styled-components'
+import './workExperience.css'
+import { experience } from '../../utils/experienceData'
 
 const WorkExperience = () => {
+
+    const currentTheme = useContext(ThemeContext)
+
+
+    console.log(experience['title'])
+
     return (
-        <div>
-            WorkExperience
-        </div>
+        <Grid container xs={12} style={{
+            
+        }}>
+
+            <Grid item xs={12}  sm={12} md={6} className='workExperienceImageGrid' >
+                <ExperienceImg theme={currentTheme} />
+            </Grid>
+
+            <Grid item xs={12} sm={12} md={6} className='workExperienceTextGrid'>
+                <h1
+                    className="workExperienceTextGridHeadingText"
+                    style={{ color: currentTheme.contrast_color }}
+                >
+                    {experience['title']}
+                   
+                </h1>
+                <h3
+                    className="workExperienceTextGridSubHeadingText"
+                    style={{ color: currentTheme.contrast_color }}
+                >
+                    {experience["subtitle"]}
+                   
+                </h3>
+                <p
+                    className="workExperienceTextGridDescription"
+                    style={{ color: currentTheme.contrast_color }}
+                >
+                    {experience["description"]}
+                    <a className="workExperienceTextGridDescriptionCompanyURL" href={experience['currentCompanyWebsiteLink']}>
+                        {experience['currentCompanyName']}
+                    </a>
+                </p>
+            </Grid>
+
+        </Grid>
     )
 }
 
---
+++ src/react_portfolio_1/utils/experienceData.js
@@ -0,0 +1,11 @@
+export const experience = {
+
+    title : "Experience",
+    subtitle : "Work and Internships",
+    description : "💼 I'm currently working as a Back End Engineer at ",
+    currentCompanyName : "Cerebranium",
+    currentCompanyWebsiteLink : "https://cerebranium.com/"
+
+}
+
+
