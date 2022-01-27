a7c2ad17d2903e457c73f68e05f93938136e0a1c
Tarang <tarangnair98@gmail.com>
2022-01-28 00:19:05 +0530

    created th experience section in the accordian

--- src/react_portfolio_1/utils/experienceData.js
+++ src/react_portfolio_1/utils/experienceData.js
@@ -13,13 +13,75 @@ export const experience = {
         {
           title: "Work Experience",
           experiences: [
-           
+            {
+              title: "Software Development Engineer 2",
+              company: "Cerebranium",
+              company_url: "https://cerebranium.com/",
+              logo_path: "cerebraniumLogo.png",
+              duration: "Nov 2020 - Present",
+              description:[
+                {
+                  icon:`🔐`,
+                  text:`Currently working on an offline mobile first exam conduction platform, leveraging AI to increase the effeciency of the institute while maintaining the exam integrity and security.`
+                },
+                {
+                  icon:`☁️`,
+                  text:`Developed and maintained API's for the mobile applications in typescript following Uncle Bob's Clean Architecture principles and deployed them as Google cloud functions.`
+                },
+                {
+                  icon:`📌`,
+                  text:`Designed firestore data models to satisfy product requirements keeping forward compatibility in mind.`
+                },
+                {
+                  icon:`🖥️`,
+                  text:`Automated complete onboarding workflows for users on our apps.`
+                },
+                {
+                  icon:`⚙️`,
+                  text:`Designed and maintained robust CI/CD pipelines to automate DevOps (for the mobile applications and cloud functions).`
+                },
+                {
+                  icon:`📈`,
+                  text:`Handled Error Monitoring.`
+                },
+                
+              ],
+            },
           ],
         },
         {
           title: "Internships",
           experiences: [
-            
+            {
+              title: "Front End Engineer Intern",
+              company: "Cerebranium",
+              company_url: "https://cerebranium.com/",
+              logo_path: "cerebraniumLogo.png",
+              duration: "July 2020 - Nov 2020",
+              description:[
+                 
+                {
+                  icon:`📱`,
+                  text:`Built two scalable cross-platform mobile applications Promexa and Promexa Institution in Flutter which are currently live on the app store and play store.`,
+                }
+                
+              ],
+            },
+            {
+              title: "Java Software Developer Intern",
+              company: "Godrej InfoTech",
+              company_url: "https://www.godrejinfotech.com/",
+              logo_path: "godrejInfoTechLogo.svg",
+              duration: "June 2019 - July 2019",
+              description:[
+                 
+                {
+                  icon:`🌐`,
+                  text:`Developed Web Applications in Java`,
+                }
+                
+              ],
+            },
           ],
         },
       ],
