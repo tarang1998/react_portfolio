acc6a44539efe625db055f46907d9efb5fa4f8c4
Tarang <tarangnair98@gmail.com>
2022-01-29 14:12:17 +0530

    Created education heading image

--- src/react_portfolio_1/pages/education/education.js
+++ src/react_portfolio_1/pages/education/education.js
@@ -1,14 +1,33 @@
+import { Grid } from "@material-ui/core";
 import React from "react";
+import EducationImg from "../../utils/educationSVGs/educationImg";
+import './education.css'
+
+import { useContext } from 'react'
+import { ThemeContext } from 'styled-components'
 
 
 
 const Education = () => {
 
+  const currentTheme = useContext(ThemeContext)
+
 
   return (
-    <>
-     
-    </>
+    <div class='educationPage'>
+      <Grid container className="educationHeading">
+
+        <Grid item xs={12} md={5} className="educationHeadingImage">
+            <EducationImg theme={currentTheme}/>
+        </Grid>
+
+        <Grid item xs={12} md={7} className="educationHeadingText">
+          
+          
+        </Grid>
+
+      </Grid> 
+    </div>
   );
 };
 
